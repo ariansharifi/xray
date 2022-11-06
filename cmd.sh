1388d1d94a323d8d19b22b5e1f7bd397c6da229e
Ariyan Sharifi <ariyansharifi@gmail.com>
2022-11-06 04:37:28 +0100

    easier config

--- cmd.sh
+++ cmd.sh
@@ -5,13 +5,13 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce
-sudo apt install python3-pip -y
+sudo apt install iftop -y
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-linux-`uname -m`" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo apt install iftop
+
 # sudo sysctl -w net.ipv4.tcp_max_orphans=262143
 # sudo sysctl -w net.ipv4.tcp_mem="370230 497642 740460" 
 
-cd ./xray
+
 sudo docker-compose up -d
 
